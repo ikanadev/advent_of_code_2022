9698262a6b552346ead5d1d65cc1d264a2b38635
Kevin <vargaskevv@gmail.com>
2022-12-06 06:31:43 -0400

    Solve day3

--- r03/src/main.rs
+++ r03/src/main.rs
@@ -15,15 +15,10 @@ fn get_numeric_value(char: char) -> u32 {
     0
 }
 
-// a = 97, A = 65
-fn main() {
-    // let file = File::open("input_small").unwrap();
-    let file = File::open("input").unwrap();
-    let reader = BufReader::new(file);
+fn part1(lines: Vec<String>) {
     let mut sum: u32 = 0;
-    for line in reader.lines() {
+    for line in lines {
         // split line
-        let line = line.unwrap();
         let half = &line[0..line.len() / 2];
         let rest = &line[line.len() / 2..];
         // save chars count to hashmap
@@ -52,4 +47,45 @@ fn main() {
         }
     }
     println!("Part1: {}", sum);
+}
+
+fn part2(lines: Vec<String>) {
+    let mut counter = 1;
+    let mut sum: u32 = 0;
+    let mut chars: HashMap<char, u32> = HashMap::new();
+    for line in lines {
+        let mut chars_set: HashSet<char> = HashSet::new();
+        for char in line.chars() {
+            chars_set.insert(char);
+        }
+        for char in chars_set {
+            chars.entry(char).and_modify(|c| *c += 1).or_insert(1);
+        }
+        if counter == 3 {
+            counter = 0;
+            for (char, count) in &chars {
+                if count == &3 {
+                    sum += get_numeric_value(*char);
+                }
+            }
+            chars = HashMap::new();
+        }
+        counter += 1;
+    }
+    println!("Part2: {}", sum);
+}
+
+// a = 97, A = 65
+fn main() {
+    // let file = File::open("input_small").unwrap();
+    let file = File::open("input").unwrap();
+    let reader = BufReader::new(file);
+    let mut lines = Vec::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let line = String::from(line);
+        lines.push(line.clone());
+    }
+    part1(lines.clone());
+    part2(lines.clone());
 }
