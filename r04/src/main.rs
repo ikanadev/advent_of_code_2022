912e78f301df2caceed652df9a850af20454a5d7
Kevin <vargaskevv@gmail.com>
2022-12-06 10:09:23 -0400

    Solve day4

--- r04/src/main.rs
+++ r04/src/main.rs
@@ -8,7 +8,7 @@ struct Section {
     to: u32,
 }
 
-fn is_section_contained(s1: Section, s2: Section) -> bool {
+fn are_sections_contained(s1: Section, s2: Section) -> bool {
     if s1.from <= s2.from && s1.to >= s2.to {
         return true;
     }
@@ -16,6 +16,16 @@ fn is_section_contained(s1: Section, s2: Section) -> bool {
         return true;
     }
     false
+}
+
+fn are_sections_overlaped(s1: Section, s2: Section) -> bool {
+    if s1.from < s2.from && s1.to < s2.from {
+        return false;
+    }
+    if s2.from < s1.from && s2.to < s1.from {
+        return false;
+    }
+    true
 }
 
 fn parse_section(str: String) -> Section {
@@ -36,21 +46,34 @@ fn part_1(lines: Vec<String>) {
         let (one, two) = parse_line(line);
         let section_one = parse_section(one);
         let section_two = parse_section(two);
-        if is_section_contained(section_one, section_two) {
+        if are_sections_contained(section_one, section_two) {
             count += 1;
         }
     }
     println!("Part1: {}", count);
 }
+fn part_2(lines: Vec<String>) {
+    let mut count: u32 = 0;
+    for line in lines {
+        let (one, two) = parse_line(line);
+        let section_one = parse_section(one);
+        let section_two = parse_section(two);
+        if are_sections_overlaped(section_one, section_two) {
+            count += 1;
+        }
+    }
+    println!("Part2: {}", count);
+}
 
 fn main() {
-    // let file = File::open("input_small").unwrap();
-    let file = File::open("input").unwrap();
+    let file = File::open("input_small").unwrap();
+    // let file = File::open("input").unwrap();
     let reader = BufReader::new(file);
     let mut lines: Vec<String> = Vec::new();
     for line in reader.lines() {
         let line = line.unwrap();
         lines.push(line);
     }
-    part_1(lines);
+    part_1(lines.clone());
+    part_2(lines.clone());
 }
